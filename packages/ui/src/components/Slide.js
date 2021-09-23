3512e058efa0c80d3356ce6c15971ad9ef6c1a77
Matthew Halloran <mdhalloran@yahoo.com>
2021-09-22 23:44:12 -0400

    Update Slide display
    -Slide now displays images better
    -Slide can now display lists of text

--- packages/ui/src/components/Slide.js
+++ packages/ui/src/components/Slide.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import {
     Box,
     Button,
@@ -19,8 +19,8 @@ const useStyles = makeStyles((theme) => ({
     },
     slideBackground: {
         position: 'absolute',
-        width: '100vw',
-        height: '100vh',
+        width: '100%',
+        height: '100%',
         objectFit: 'cover',
     },
     slidePad: {
@@ -35,6 +35,23 @@ const useStyles = makeStyles((theme) => ({
     },
     mainGrid: {
         paddingBottom: '2em',
+    },
+    bodyGridItem: {
+        paddingBottom: '1em',
+    },
+    bodyList: {
+        listStyleType: 'circle',
+        fontSize: 'xxx-large',
+    },
+    bodyImageContainer: {
+        justifyContent: 'center',
+        height: '100%',
+        display: 'flex',
+    },
+    bodyImage: {
+        maxWidth: '90%',
+        maxHeight: '100%',
+        objectFit: 'contain',
     },
     buttonCenter: {
         display: 'flex',
@@ -55,10 +72,12 @@ const useStyles = makeStyles((theme) => ({
         slideRoot: {
             minHeight: '50vh',
         },
-        slideBackground: {
-            height: '50vh',
-        }
     },
+    [theme.breakpoints.down('sm')]: {
+        bodyImage: {
+            maxWidth: '75%',
+        },
+    }
 }))
 
 function Slide({
@@ -68,54 +87,69 @@ function Slide({
     const classes = useStyles();
     const history = useHistory();
 
-    // Background can either be a solid color or an image
-    const toBackground = (background) => {
-        console.log('in to background', background?.image?.src)
-        if (!background) return null;
-        if (background.image) {
-            return <img className={classes.slideBackground} alt={background?.image?.alt} src={background?.image?.src} />
+    // Background can either be an image, gradient, or solid color
+    const backgroundStyle = useMemo(() => {
+        if (!data.background) return null;
+        if (data.background.image) {
+            console.log('SETTIG BACKGROUND IMAGE', `url("${data.background.image}") no-repeat center center cover`)
+            return {
+                background: `url(${data.background.image}) no-repeat center center`,
+                backgroundSize: 'cover',
+            }
         }
-        // TODO solid color option
-        return null;
-    }
+        return data.background;
+    }, [data])
 
     const toTitle = (title) => {
         if (!title) return null;
         let titleClasses = `${classes.titleCenter}`;
         if (title.style === 'pop') titleClasses += ` ${classes.textPop}`;
-        return <Typography variant='h2' component='h1' className={titleClasses}>{title.text}</Typography>
+        return <Typography variant='h2' component='h1' className={titleClasses} style={{ color: title.color }}>{title.text}</Typography>
     }
 
     const toBodyChild = useCallback((child) => {
-        console.log('in body child', child)
         if (!child) return null;
         // If child has 'content' property, then it is a GridItem
         if (Array.isArray(child.content)) {
-            return <Grid item xs={child.xs ?? 4}>
+            return <Grid className={classes.bodyGridItem} item xs={child.xs ?? 12} sm={child.sm}>
                 {child.content.map(grandchild => toBodyChild(grandchild))}
             </Grid>
         }
-        console.log('here', child)
         // If child has 'title' property, then it is a Typography
         if (child.title) return (
-            <Typography 
-                variant={child.title.variant ?? 'h4'} 
-                component={child.title.component ?? 'h2'} 
+            <Typography
+                variant={child.title.variant ?? 'h4'}
+                component={child.title.component ?? 'h2'}
                 textAlign={child.title.textAlign ?? 'left'}
+                color={child.title.color}
             >
                 {child.title.text}
             </Typography>
         )
+        // If child has 'list' property, then it is a list of Typography
+        if (child.list) return (
+            <ul>
+                {child.list.items.map(item => (
+                    <li className={classes.bodyList} style={{color: item.color ?? child.list.color}}><Typography
+                    variant={item.variant ?? child.list.variant ?? 'h4'}
+                    component={item.component ?? child.list.component ?? 'h2'}
+                    textAlign={item.textAlign ?? child.list.textAlign ?? 'left'}
+                    color={item.color ?? child.list.color}
+                >
+                    {item.text}
+                </Typography></li>
+                ))}
+            </ul>
+        )
         // If child has 'video' property, then it is a YoutubeEmbed
-        if (child.video) return <YoutubeEmbed embedId={child.video.link} width={Math.min(width-16, 1000)} height={Math.floor(Math.min(width-16, 1000)*9/16)} />
+        if (child.video) return <YoutubeEmbed embedId={child.video.link} width={Math.min(width - 16, 600)} height={Math.floor(Math.min(width - 16, 600) * 9 / 16)} />
         //TODO
-        if (child.image) return null;
+        if (child.image) return <div className={classes.bodyImageContainer} ><img className={classes.bodyImage} alt={child.image.alt} src={child.image.src} /></div>
         //TODO
         return null;
     }, [width])
 
     const toBody = (body) => {
-        console.log('in body', body)
         if (!Array.isArray(body)) return null;
         return <Grid className={classes.mainGrid} container spacing={1}>
             {body.map(child => toBodyChild(child))}
@@ -130,21 +164,23 @@ function Slide({
             type="submit"
             size="large"
             color={button.color ?? 'secondary'}
-            onClick={() => history.push(button.link)}
+            onClick={() => button.link.includes('http') ? window.open(button.link, '_blank', 'noopener,noreferrer') : history.push(button.link)}
         >
             {button.text}
         </Button>
     }
 
+    console.log('STYLES', backgroundStyle)
+
     return (
-        <Box className={classes.slideRoot}>
-            {toBackground(data?.background)}
+        <div className={classes.slideRoot} 
+            style={{...backgroundStyle}}>
             <div className={classes.slidePad}>
                 {toTitle(data?.title)}
                 {toBody(data?.body)}
                 {toButton(data?.button)}
             </div>
-        </Box>
+        </div>
     );
 }
 
