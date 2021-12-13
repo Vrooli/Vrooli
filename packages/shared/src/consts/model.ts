f20e1b26ba9eb2317536a1fb43ae396f22084316
Matthew Halloran <mdhalloran@yahoo.com>
2021-12-13 16:31:05 -0500

    Database p1
    Added the majority of database rows required for the MVP

--- packages/shared/src/consts/model.ts
+++ packages/shared/src/consts/model.ts
@@ -13,3 +13,69 @@ export const ROLES = {
     Guest: 'Guest',
 }
 export type ROLES = ValueOf<typeof ROLES>;
+
+// ============================================================
+// Node Types
+// ============================================================
+
+export const NODE_TYPES = {
+    Combine: 'combine',
+    Decision: 'decision',
+    End: 'end',
+    Loop: 'loop',
+    RoutineList: 'list',
+    Redirect: 'redirect',
+    Start: 'start',
+}
+export type NODE_TYPES = ValueOf<typeof NODE_TYPES>;
+
+export type COMBINE_NODE_DATA = null // TODO: define combine data
+
+export type DECISION_NODE_DATA = {
+    title: string,
+    description: string | null,
+    decisions: Array<{
+        id: string,
+        title: string,
+        next: string,
+        when: Array<any> //TODO: define
+    }>
+}
+
+export type END_NODE_DATA = {
+    title: string,
+    description: string | null,
+    wasSuccessful: boolean,
+}
+
+export type LOOP_NODE_DATA = null; //TODO: define loop data
+
+export type ROUTINE_LIST_NODE_DATA = {
+    title: string,
+    description: string | null,
+    isOrdered: boolean,
+    routines: Array<{
+        id: string,
+        title: string,
+        description: string | null,
+        isOptional: boolean,
+    }>
+};
+
+export type REDIRECT_NODE_DATA = null; // Supposed to be null
+
+export type START_NODE_DATA = null; // Supposed to be null
+
+export type NODE_DATA = {
+    id: string,
+    type: NODE_TYPES,
+    previous: string | null,
+    next: string | null,
+    data: COMBINE_NODE_DATA | DECISION_NODE_DATA | END_NODE_DATA | LOOP_NODE_DATA | ROUTINE_LIST_NODE_DATA | REDIRECT_NODE_DATA | START_NODE_DATA,
+}
+
+export type ORCHESTRATION_DATA = {
+    title: string,
+    description?: string,
+    nodes: Array<NODE_DATA>,
+}
--- packages/ui/src/pages/RoutineOrchestratorPage.tsx
+++ packages/ui/src/pages/RoutineOrchestratorPage.tsx
@@ -1,8 +1,188 @@
 import { makeStyles } from '@material-ui/styles';
-import { Theme } from '@material-ui/core';
+import { Theme, Typography } from '@material-ui/core';
+import { NODE_TYPES, ORCHESTRATION_DATA } from '@local/shared';
+
+//TEMP
+const data: ORCHESTRATION_DATA = {
+    title: 'Validate business idea',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    // Unordered list of top-level nodes
+    // Page renders nodes starting at the start node
+    nodes: [
+        // Start node
+        {
+            id: '1',
+            type: NODE_TYPES.Start,
+            // ID of previous node
+            previous: null,
+            // ID of next node
+            next: '2',
+            // Additional data specific to the node type
+            data: null
+        },
+        // Routine List node
+        {
+            id: '2',
+            type: NODE_TYPES.RoutineList,
+            previous: '1',
+            next: '3',
+            data: {
+                title: 'Provide Basic Info',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+                isOrdered: false,
+                routines: [
+                    {
+                        id: '1',
+                        title: 'Provide Basic Info',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+                        isOptional: false,
+                    }
+                ]
+            }
+        },
+        // Routine List node
+        {
+            id: '3',
+            type: NODE_TYPES.RoutineList,
+            previous: '2',
+            next: '4',
+            data: {
+                title: 'Knowledge management',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+                isOrdered: false,
+                routines: [
+                    {
+                        id: '2',
+                        title: 'Create task list',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+                        isOptional: true,
+                    },
+                    {
+                        id: '3',
+                        title: 'Generate forecasts',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+                        isOptional: true,
+                    },
+                    {
+                        id: '4',
+                        title: 'List objectives',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+                        isOptional: false,
+                    },
+                    {
+                        id: '5',
+                        title: 'Create business model',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+                        isOptional: true,
+                    }
+                ]
+            }
+        },
+        // Decision node
+        {
+            id: '4',
+            type: NODE_TYPES.Decision,
+            previous: '3',
+            // Decision nodes have no definitive next node
+            // Instead, next node is determined by the decision
+            next: null,
+            data: {
+                title: 'Worth pursuing?',
+                description: null,
+                decisions: [
+                    {
+                        id: '1',
+                        title: 'Yes',
+                        next: '5',
+                        // List of cases which must return true in order for this decision to be available
+                        // If empty, decision is always available
+                        when: []
+                    },
+                    {
+                        id: '2',
+                        title: 'No',
+                        next: '6',
+                        when: []
+                    }
+                ]
+            }
+        },
+        // Routine List node
+        {
+            id: '5',
+            type: NODE_TYPES.RoutineList,
+            previous: '4',
+            next: '7',
+            data: {
+                title: 'todo',
+                description: null,
+                isOrdered: false,
+                routines: []
+            },
+        },
+        // End node
+        {
+            id: '6',
+            type: NODE_TYPES.End,
+            previous: '4',
+            next: null,
+            data: {
+                wasSuccessful: true,
+            }
+        },
+        // Decision node
+        {
+            id: '7',
+            type: NODE_TYPES.Decision,
+            previous: '4',
+            next: null,
+            data: {
+                title: 'Try again?',
+                description: null,
+                decisions: [
+                    {
+                        id: '3',
+                        title: 'Yes',
+                        next: '8',
+                        // List of cases which must return true in order for this decision to be available
+                        // If empty, decision is always available
+                        when: []
+                    },
+                    {
+                        id: '4',
+                        title: 'No',
+                        next: '9',
+                        when: []
+                    }
+                ]
+            }
+        },
+        // Redirect node
+        {
+            id: '8',
+            type: NODE_TYPES.Redirect,
+            previous: '7',
+            next: '1',
+            data: null,
+        },
+        // End node
+        {
+            id: '9',
+            type: NODE_TYPES.End,
+            previous: '7',
+            next: null,
+            data: {
+                wasSuccessful: false,
+            }
+        },
+    ]
+}
 
 const useStyles = makeStyles((theme: Theme) => ({
-    root: {}
+    root: {},
+    title: {
+        textAlign: 'center',
+    },
 }));
 
 export const RoutineOrchestratorPage = () => {
@@ -11,7 +191,7 @@ export const RoutineOrchestratorPage = () => {
 
     return (
         <div id="page" className={classes.root}>
-            
+            <Typography component="h2" variant="h4" className={classes.title}>{data.title}</Typography>
         </div>
     )
 };
