75f72bda6b2f1e758c9710e2d6c0d4388b253c07
Matthew Halloran <mdhalloran@yahoo.com>
2022-01-16 21:31:45 -0500

    Bad words filter
    Added ability to filter bad words

---
+++ packages/server/src/utils/censor.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+
+const profanity = fs.readFileSync(`${process.env.PROJECT_DIR}/packages/server/src/utils/censorDictionary.txt`).toString().split("\n");
+const profanityRegex = new RegExp(profanity.join('|'), 'gi');
+
+/**
+ * Determines if a string contains any banned words
+ * @param text The text which may contain bad words
+ * @returns True if any bad words were found
+ */
+export const hasProfanity = (text: string): boolean => {
+    return text.search(profanityRegex) !== -1;
+}
+
+/**
+ * Removes profanity from a string
+ * @param text The text to censor
+ * @param censorCharacter The character to replace profanity with
+ * @returns The censored text
+ */
+export const filterProfanity = (text: string, censorCharacter: string = '*'): string => {
+    return text.replace(profanityRegex, (s: string) => {
+        var i = 0;
+        var asterisks = '';
+
+        while (i < s.length) {
+            asterisks += censorCharacter;
+            i++;
+        }
+
+        return asterisks;
+    });
+}
