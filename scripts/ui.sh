0772a3d3c9562d0159b7aae7c26cc06dc6331f78
Matthew Halloran <mdhalloran@yahoo.com>
2022-05-24 11:02:50 -0400

    Developer icons
    Added icons to indicate when the development version of the site is being run. This allows me to download both versions without getting confused👍

--- scripts/ui.sh
+++ scripts/ui.sh
@@ -5,11 +5,14 @@ ${PROJECT_DIR}/scripts/wait-for.sh server:5329 -t 1000 -- echo 'Backend server i
 
 cd ${PROJECT_DIR}/packages/ui
 
-# Production build extra steps
-# if [ "${NODE_ENV}" = "production" ]; then
-#     # Build project
-#     yarn build
-# fi
+# Development build extra steps
+if [ "${NODE_ENV}" = "development" ]; then
+    # Replace favicon images with the development version. 
+    # This makes it easier to see which downloaded version of the site is which. 
+    # NOTE: This does not specify if the build is run locally or on a server. If you really 
+    # want to get fancy with it, you could make a set of images for these cases.
+    mv ${PROJECT_DIR}/packages/ui/public/dev/* ${PROJECT_DIR}/packages/ui/public/
+fi
 
 # Finally, start project
 yarn start-${NODE_ENV}
