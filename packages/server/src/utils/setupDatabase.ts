9f6570ee7e73a58d890cc7a8e31b713c7a649b9e
MattHalloran <matthalloran8@gmail.com>
2022-09-26 17:47:29 -0400

    Formatted dockerfiles
    Dockerfiles now format correctly when the format command is triggered in VSCode

--- packages/server/src/utils/setupDatabase.ts
+++ packages/server/src/utils/setupDatabase.ts
@@ -7,7 +7,7 @@ import { PrismaType } from '../types';
 const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
 
-const executeSeed = async (func: (prisma: PrismaType) => any, exitOnFail = false,) => {
+const executeSeed = async (func: (prisma: PrismaType) => any, exitOnFail = false) => {
     await func(prisma).catch((error: any) => {
         logger.log(LogLevel.error, 'Database seed caught error', { code: genErrorCode('00011'), error });
         if (exitOnFail) process.exit(1);
