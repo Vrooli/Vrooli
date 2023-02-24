6b697531109651c8973209dd2d9ca2abf82cb67a
MattHalloran <matthalloran8@gmail.com>
2023-02-24 17:13:09 -0500

    useLazyQuery optional query
    useLazyQuery now supports passing in an undefined query. This is useful for search pages, where the query is found from an async import.
    
    In other words, useLazyQuery now supports using a lazy query😄

--- packages/ui/src/api/hooks/useLazyQuery.ts
+++ packages/ui/src/api/hooks/useLazyQuery.ts
@@ -3,9 +3,10 @@
  */
 import { TypedDocumentNode } from '@graphql-typed-document-node/core';
 import { useCallback, useMemo, useRef } from 'react';
-import { DocumentNode, LazyQueryHookOptions, LazyQueryResultTuple, mergeOptions, OperationVariables, QueryResult, useApolloClient } from '@apollo/client';
+import { DocumentNode, LazyQueryHookOptions, LazyQueryResultTuple, mergeOptions, Operation, OperationVariables, QueryResult, useApolloClient } from '@apollo/client';
 import { useInternalState } from '@apollo/client/react/hooks/useQuery';
 import { IWrap, Wrap } from 'types';
+import { exists } from '@shared/utils';
 
 // The following methods, when called will execute the query, regardless of
 // whether the useLazyQuery execute function was called before.
@@ -18,27 +19,40 @@ const EAGER_METHODS = [
     'subscribeToMore',
 ] as const;
 
+type UseLazyQueryResult<TData extends object | boolean | number | string | undefined, TVariables extends OperationVariables | undefined, Endpoint extends string = string> =
+    TData extends undefined ?
+    [() => {}, { data: Record<string, any>, loading: false }] : // Dummy data
+    TVariables extends OperationVariables ?
+    [Wrap<TData, Endpoint>, QueryResult<Wrap<TData, Endpoint>, TVariables>] :
+    [Wrap<TData, Endpoint>, QueryResult<Wrap<TData, Endpoint>>];
+
+type UseLazyQueryParams<TData extends object | boolean | number | string | undefined, TVariables extends OperationVariables | undefined, Endpoint extends string = string> = {
+    query: DocumentNode | TypedDocumentNode<TData, TVariables> | undefined;
+    endpoint: Endpoint;
+    options?: TData extends undefined ? {} : TVariables extends OperationVariables ? LazyQueryHookOptions<TData, TVariables> : LazyQueryHookOptions<TData>;
+}
+
 export function useLazyQuery<
     TData extends object | boolean | number | string,
-    TVariables extends OperationVariables | null,
+    TVariables extends OperationVariables | undefined,
     Endpoint extends string = string
->(
-    query: DocumentNode | TypedDocumentNode<TData, TVariables>,
-    endpoint: Endpoint, // Only used for type inference
-    options?: LazyQueryHookOptions<TData, TVariables>
-): LazyQueryResultTuple<Wrap<TData, Endpoint>, TVariables> {
+>({
+    query,
+    endpoint, // Only used for type inference
+    options,
+}: UseLazyQueryParams<TData, TVariables, Endpoint>): UseLazyQueryResult<TData, TVariables, Endpoint> {
     const { variables, ...rest } = options || {};
     // If variables are provided (i.e. not undefined), wrap them in "input" to match the schema. We use this 
     // to avoid having to define the input fields in the DocumentNode definitions
     const opts = useMemo(() => ({ ...rest, variables: variables ? { input: variables } : undefined }), [rest, variables]);
     const internalState = useInternalState<Wrap<TData, Endpoint>, IWrap<TVariables>>(
         useApolloClient(opts && opts.client),
-        query,
+        query ?? ({ kind: 'Document', definitions: [] } as DocumentNode),
     );
 
     const execOptionsRef = useRef<Partial<LazyQueryHookOptions<Wrap<TData, Endpoint>, IWrap<TVariables>>>>();
     const merged = execOptionsRef.current
-        ? mergeOptions(opts, execOptionsRef.current)
+        ? mergeOptions(opts, execOptionsRef.current as OperationVariables)
         : opts;
 
     const useQueryResult = internalState.useQuery({
@@ -50,7 +64,7 @@ export function useLazyQuery<
         useQueryResult.observable.options.initialFetchPolicy ||
         internalState.getDefaultFetchPolicy();
 
-    const result: QueryResult<Wrap<TData, Endpoint>, IWrap<TVariables>> =
+    const result =
         Object.assign(useQueryResult, {
             called: !!execOptionsRef.current,
         });
@@ -71,13 +85,13 @@ export function useLazyQuery<
         }
 
         return eagerMethods;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     Object.assign(result, eagerMethods);
 
     const execute = useCallback<
-        LazyQueryResultTuple<Wrap<TData, Endpoint>, TVariables>[0]
+        LazyQueryResultTuple<Wrap<TData, Endpoint>, OperationVariables>[0]
     >(executeOptions => {
         execOptionsRef.current = (executeOptions ? {
             ...executeOptions,
@@ -88,7 +102,7 @@ export function useLazyQuery<
             fetchPolicy: initialFetchPolicy,
         }) as any;
 
-        const promise = internalState
+        const promise = (internalState as any)
             .asyncUpdate() // Like internalState.forceUpdate, but returns a Promise.
             .then(queryResult => Object.assign(queryResult, eagerMethods));
 
@@ -97,8 +111,13 @@ export function useLazyQuery<
         promise.catch(() => { });
 
         return promise as any;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    // If the query is undefined, return a dummy function and dummy data
+    if (!exists(query)) {
+        return [() => { }, { data: {}, loading: false }] as any;
+    }
+    // Otherwise, return the actual data
     return [execute, result] as any;
 }
