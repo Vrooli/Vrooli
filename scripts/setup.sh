1c8e50b9ac66e86edf0de8f895a1ac05cdb41c46
MattHalloran <matthalloran8@gmail.com>
2023-02-28 10:52:37 -0500

    yarn v1 -> v3
    Upgraded yarn to version 3

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -68,6 +68,7 @@ nvm alias default v16.16.0
 
 header "Installing Yarn"
 npm install -g yarn
+yarn set version berry
 
 header "Installing global dependencies"
 yarn global add apollo@2.34.0 typescript ts-node nodemon prisma@4.10.1 vite
