6627df55794622d176770ed83cef38368a4c5290
MattHalloran <matthalloran8@gmail.com>
2023-04-12 01:09:18 -0400

    Update server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -9,7 +9,7 @@ if [ "${NODE_ENV}" = "development" ]; then
 fi
 
 # info 'Waiting for database and redis to start...'
-${PROJECT_DIR}/scripts/wait-for.sh 5432 -t 120 -- echo 'Database is up'
+${PROJECT_DIR}/scripts/wait-for.sh redis:5432 -t 120 -- echo 'Database is up'
 # ${PROJECT_DIR}/scripts/wait-for.sh ${REDIS_CONN} -t 60 -- echo 'Redis is up'
 
 PRISMA_SCHEMA_FILE="src/db/schema.prisma"
