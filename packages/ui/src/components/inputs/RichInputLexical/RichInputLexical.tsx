9f8f1dcf94680a4e9c145e6b9fbb856cccf829ca
MattHalloran <matthalloran8@gmail.com>
2023-08-26 21:58:09 -0400

    Lexical strikethrough and theme
    - Added strikethrough support
    - Added themes from playground, which will later be updated as best as I can to match MarkdownDisplay

--- packages/ui/src/components/inputs/RichInputLexical/RichInputLexical.tsx
+++ packages/ui/src/components/inputs/RichInputLexical/RichInputLexical.tsx
@@ -23,13 +23,15 @@ import { $isTableNode } from "@lexical/table";
 import { $findMatchingParent, $getNearestNodeOfType } from "@lexical/utils";
 import { Box, useTheme } from "@mui/material";
 import "highlight.js/styles/monokai-sublime.css";
-import { $createParagraphNode, $getRoot, $getSelection, $isElementNode, $isRangeSelection, $isRootOrShadowRoot, COMMAND_PRIORITY_CRITICAL, createEditor, DEPRECATED_$isGridSelection, ElementFormatType, ElementNode, FORMAT_TEXT_COMMAND, LexicalEditor, NodeKey, RangeSelection, SELECTION_CHANGE_COMMAND, TextNode } from "lexical";
+import type { EditorThemeClasses } from "lexical";
+import { $createParagraphNode, $getRoot, $getSelection, $isElementNode, $isRangeSelection, $isRootOrShadowRoot, COMMAND_PRIORITY_CRITICAL, createEditor, DEPRECATED_$isGridSelection, ElementFormatType, ElementNode, FORMAT_TEXT_COMMAND, LexicalEditor, NodeKey, RangeSelection, SELECTION_CHANGE_COMMAND, TextFormatType, TextNode } from "lexical";
 import { CSSProperties, useCallback, useEffect, useLayoutEffect, useMemo, useState } from "react";
 import { linkColors } from "styles";
 import { ListObject } from "utils/display/listTools";
 import { RichInputTagDropdown, useTagDropdown } from "../RichInputTagDropdown/RichInputTagDropdown";
 import { RichInputAction } from "../RichInputToolbar/RichInputToolbar";
 import { RichInputChildView, RichInputLexicalProps } from "../types";
+import "./theme.css";
 
 export const CAN_USE_DOM: boolean =
     typeof window !== "undefined" &&
@@ -57,8 +59,105 @@ const rootTypeToRootName = {
     table: "Table",
 };
 
-const theme = {
-
+const theme: EditorThemeClasses = {
+    blockCursor: "PlaygroundEditorTheme__blockCursor",
+    characterLimit: "PlaygroundEditorTheme__characterLimit",
+    code: "PlaygroundEditorTheme__code",
+    codeHighlight: {
+        atrule: "PlaygroundEditorTheme__tokenAttr",
+        attr: "PlaygroundEditorTheme__tokenAttr",
+        boolean: "PlaygroundEditorTheme__tokenProperty",
+        builtin: "PlaygroundEditorTheme__tokenSelector",
+        cdata: "PlaygroundEditorTheme__tokenComment",
+        char: "PlaygroundEditorTheme__tokenSelector",
+        class: "PlaygroundEditorTheme__tokenFunction",
+        "class-name": "PlaygroundEditorTheme__tokenFunction",
+        comment: "PlaygroundEditorTheme__tokenComment",
+        constant: "PlaygroundEditorTheme__tokenProperty",
+        deleted: "PlaygroundEditorTheme__tokenProperty",
+        doctype: "PlaygroundEditorTheme__tokenComment",
+        entity: "PlaygroundEditorTheme__tokenOperator",
+        function: "PlaygroundEditorTheme__tokenFunction",
+        important: "PlaygroundEditorTheme__tokenVariable",
+        inserted: "PlaygroundEditorTheme__tokenSelector",
+        keyword: "PlaygroundEditorTheme__tokenAttr",
+        namespace: "PlaygroundEditorTheme__tokenVariable",
+        number: "PlaygroundEditorTheme__tokenProperty",
+        operator: "PlaygroundEditorTheme__tokenOperator",
+        prolog: "PlaygroundEditorTheme__tokenComment",
+        property: "PlaygroundEditorTheme__tokenProperty",
+        punctuation: "PlaygroundEditorTheme__tokenPunctuation",
+        regex: "PlaygroundEditorTheme__tokenVariable",
+        selector: "PlaygroundEditorTheme__tokenSelector",
+        string: "PlaygroundEditorTheme__tokenSelector",
+        symbol: "PlaygroundEditorTheme__tokenProperty",
+        tag: "PlaygroundEditorTheme__tokenProperty",
+        url: "PlaygroundEditorTheme__tokenOperator",
+        variable: "PlaygroundEditorTheme__tokenVariable",
+    },
+    embedBlock: {
+        base: "PlaygroundEditorTheme__embedBlock",
+        focus: "PlaygroundEditorTheme__embedBlockFocus",
+    },
+    hashtag: "PlaygroundEditorTheme__hashtag",
+    heading: {
+        h1: "PlaygroundEditorTheme__h1",
+        h2: "PlaygroundEditorTheme__h2",
+        h3: "PlaygroundEditorTheme__h3",
+        h4: "PlaygroundEditorTheme__h4",
+        h5: "PlaygroundEditorTheme__h5",
+        h6: "PlaygroundEditorTheme__h6",
+    },
+    image: "editor-image",
+    indent: "PlaygroundEditorTheme__indent",
+    link: "PlaygroundEditorTheme__link",
+    list: {
+        listitem: "PlaygroundEditorTheme__listItem",
+        listitemChecked: "PlaygroundEditorTheme__listItemChecked",
+        listitemUnchecked: "PlaygroundEditorTheme__listItemUnchecked",
+        nested: {
+            listitem: "PlaygroundEditorTheme__nestedListItem",
+        },
+        olDepth: [
+            "PlaygroundEditorTheme__ol1",
+            "PlaygroundEditorTheme__ol2",
+            "PlaygroundEditorTheme__ol3",
+            "PlaygroundEditorTheme__ol4",
+            "PlaygroundEditorTheme__ol5",
+        ],
+        ul: "PlaygroundEditorTheme__ul",
+    },
+    ltr: "PlaygroundEditorTheme__ltr",
+    mark: "PlaygroundEditorTheme__mark",
+    markOverlap: "PlaygroundEditorTheme__markOverlap",
+    paragraph: "PlaygroundEditorTheme__paragraph",
+    quote: "PlaygroundEditorTheme__quote",
+    rtl: "PlaygroundEditorTheme__rtl",
+    table: "PlaygroundEditorTheme__table",
+    tableAddColumns: "PlaygroundEditorTheme__tableAddColumns",
+    tableAddRows: "PlaygroundEditorTheme__tableAddRows",
+    tableCell: "PlaygroundEditorTheme__tableCell",
+    tableCellActionButton: "PlaygroundEditorTheme__tableCellActionButton",
+    tableCellActionButtonContainer:
+        "PlaygroundEditorTheme__tableCellActionButtonContainer",
+    tableCellEditing: "PlaygroundEditorTheme__tableCellEditing",
+    tableCellHeader: "PlaygroundEditorTheme__tableCellHeader",
+    tableCellPrimarySelected: "PlaygroundEditorTheme__tableCellPrimarySelected",
+    tableCellResizer: "PlaygroundEditorTheme__tableCellResizer",
+    tableCellSelected: "PlaygroundEditorTheme__tableCellSelected",
+    tableCellSortedIndicator: "PlaygroundEditorTheme__tableCellSortedIndicator",
+    tableResizeRuler: "PlaygroundEditorTheme__tableCellResizeRuler",
+    tableSelected: "PlaygroundEditorTheme__tableSelected",
+    text: {
+        bold: "PlaygroundEditorTheme__textBold",
+        code: "PlaygroundEditorTheme__textCode",
+        italic: "PlaygroundEditorTheme__textItalic",
+        strikethrough: "PlaygroundEditorTheme__textStrikethrough",
+        subscript: "PlaygroundEditorTheme__textSubscript",
+        superscript: "PlaygroundEditorTheme__textSuperscript",
+        underline: "PlaygroundEditorTheme__textUnderline",
+        underlineStrikethrough: "PlaygroundEditorTheme__textUnderlineStrikethrough",
+    },
 };
 
 function initializeEditor(
@@ -333,49 +432,44 @@ export const RichInputLexical = ({
     }, [composerContext, $updateToolbar]);
 
     // Toolbar actions
-    const formatHeading = (headingSize: HeadingTagType) => {
-        // If the current block is already the desired heading size, set to paragraph
-        if (blockType === headingSize) {
-            composerContext[0].update(() => {
-                const selection = $getSelection();
-                if (
-                    $isRangeSelection(selection) ||
-                    DEPRECATED_$isGridSelection(selection)
-                ) {
-                    $setBlocksType(selection, () => $createParagraphNode());
-                }
-            });
-        }
-        // Otherwise, set to the desired heading size
-        else {
-            composerContext[0].update(() => {
-                const selection = $getSelection();
-                if (
-                    $isRangeSelection(selection) ||
-                    DEPRECATED_$isGridSelection(selection)
-                ) {
-                    $setBlocksType(selection, () => $createHeadingNode(headingSize));
-                }
-            });
-        }
+    const toggleHeading = (headingSize: HeadingTagType) => {
+        composerContext[0].update(() => {
+            const selection = $getSelection();
+            if (
+                $isRangeSelection(selection) ||
+                DEPRECATED_$isGridSelection(selection)
+            ) {
+                $setBlocksType(selection, () => blockType === headingSize ? $createParagraphNode() : $createHeadingNode(headingSize));
+            }
+        });
+    };
+    const toggleFormat = (formatType: TextFormatType) => {
+        // composerContext[0].update(() => {
+        //     const selection = $getSelection();
+        //     console.log("got selection", selection, $isRangeSelection(selection));
+        //     if ($isRangeSelection(selection)) {
+        //         selection.toggleFormat(formatType);
+        //     }
+        // });
+        composerContext[0].dispatchCommand(FORMAT_TEXT_COMMAND, formatType);
     };
     (RichInputLexical as unknown as RichInputChildView).handleAction = (action: RichInputAction) => {
         console.log("in RichInputlExical handleAction", action);
         const dispatch = composerContext[0].dispatchCommand;
         const actionMap: { [key in RichInputAction]: (() => unknown) } = {
             "Assistant": () => openAssistantDialog(""),
-            "Bold": () => dispatch(FORMAT_TEXT_COMMAND, "bold"),
-            "Header1": () => formatHeading("h1"),
-            "Header2": () => formatHeading("h2"),
-            "Header3": () => formatHeading("h3"),
-            "Italic": () => dispatch(FORMAT_TEXT_COMMAND, "italic"),
+            "Bold": () => toggleFormat("bold"),
+            "Header1": () => toggleHeading("h1"),
+            "Header2": () => toggleHeading("h2"),
+            "Header3": () => toggleHeading("h3"),
+            "Italic": () => toggleFormat("italic"),
             "Link": () => dispatch(TOGGLE_LINK_COMMAND, "https://"), //TODO not working
             "ListBullet": () => dispatch(INSERT_UNORDERED_LIST_COMMAND, (void 0)),
             "ListCheckbox": () => dispatch(INSERT_CHECK_LIST_COMMAND, (void 0)), // TODO not working
             "ListNumber": () => dispatch(INSERT_ORDERED_LIST_COMMAND, (void 0)),
             "Mode": toggleMarkdown,
             "Redo": redo,
-            "Strikethrough": () => { },
+            "Strikethrough": () => toggleFormat("strikethrough"),
             "Undo": undo,
         };
         const actionFunction = actionMap[action];
