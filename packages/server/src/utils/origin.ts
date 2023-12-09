69aed8d2e011cc0d6be75d2fd8c3dccb266df580
MattHalloran <matthalloran8@gmail.com>
2023-12-08 23:46:52 -0500

    Regex injection fix
    https://github.com/Vrooli/Vrooli/security/code-scanning/18 and https://github.com/Vrooli/Vrooli/security/code-scanning/16

--- packages/server/src/utils/origin.ts
+++ packages/server/src/utils/origin.ts
@@ -1,18 +1,59 @@
 import { Request } from "express";
 
+let cachedOrigins: Array<string | RegExp> | null = null;
+
+const ipv4Regex = /^((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])$/;
+const ipv6Pattern = `
+    (
+        ([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|          # 1:2:3:4:5:6:7:8
+        ([0-9a-fA-F]{1,4}:){1,7}:|                         # 1::                              1:2:3:4:5:6:7::
+        ([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|         # 1::8             1:2:3:4:5:6::8  1:2:3:4:5:6::8
+        ([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|  # 1::7:8           1:2:3:4:5::7:8  1:2:3:4:5::8
+        ([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|  # 1::6:7:8         1:2:3:4::6:7:8  1:2:3:4::8
+        ([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|  # 1::5:6:7:8       1:2:3::5:6:7:8  1:2:3::8
+        ([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|  # 1::4:5:6:7:8     1:2::4:5:6:7:8  1:2::8
+        [0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|       # 1::3:4:5:6:7:8   1::3:4:5:6:7:8  1::8  
+        :((:[0-9a-fA-F]{1,4}){1,7}|:)|                     # ::2:3:4:5:6:7:8  ::2:3:4:5:6:7:8 ::8       ::     
+        fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|     # fe80::7:8%eth0   fe80::7:8%1     (link-local IPv6 addresses with zone index)
+        ::(ffff(:0{1,4}){0,1}:){0,1}
+        ((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}
+        (25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|          # ::255.255.255.255   ::ffff:255.255.255.255  ::ffff:0:255.255.255.255  (IPv4-mapped IPv6 addresses and IPv4-translated addresses)
+        ([0-9a-fA-F]{1,4}:){1,4}:
+        ((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}
+        (25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])           # 2001:db8:3:4::192.0.2.33  64:ff9b::192.0.2.33 (IPv4-Embedded IPv6 Address)
+    )
+    `.replace(/\s*#.*$/gm, "").replace(/\s+/g, "");
+const ipv6Regex = new RegExp(ipv6Pattern);
+
+/**
+ * Validate if the given string is a valid IP address.
+ * @param {string} ip - The IP address to validate.
+ * @returns {boolean} - True if valid, false otherwise.
+ */
+const isValidIP = (ip: string): boolean => {
+    return ipv4Regex.test(ip) || ipv6Regex.test(ip);
+};
+
 /**
  * Origins which are allowed to make requests without an API key.
  * @returns An array of strings and regular expressions
  */
 export const safeOrigins = (): Array<string | RegExp> => {
+    if (cachedOrigins !== null) {
+        return cachedOrigins;
+    }
+
     const origins: Array<string | RegExp> = [];
+    const siteIp = process.env.SITE_IP;
     if (process.env.VITE_SERVER_LOCATION === "local") {
         origins.push(
             /^http:\/\/localhost(?::[0-9]+)?$/,
             /^http:\/\/192.168.0.[0-9]{1,2}(?::[0-9]+)?$/,
             "https://studio.apollographql.com",
-            new RegExp(`^http(s)?://${process.env.SITE_IP}(?::[0-9]+)?$`),
         );
+        if (siteIp && isValidIP(siteIp)) {
+            origins.push(new RegExp(`^http(s)?://${siteIp}(?::[0-9]+)?$`));
+        }
     }
     else {
         // Parse URLs from process.env.VIRTUAL_HOST
@@ -20,10 +61,12 @@ export const safeOrigins = (): Array<string | RegExp> => {
         for (const domain of domains) {
             origins.push(new RegExp(`^http(s)?://${domain}$`));
         }
-        origins.push(
-            new RegExp(`^http(s)?://${process.env.SITE_IP}(?::[0-9]+)?$`),
-        );
+        if (siteIp && isValidIP(siteIp)) {
+            origins.push(new RegExp(`^http(s)?://${siteIp}(?::[0-9]+)?$`));
+        }
     }
+
+    cachedOrigins = origins;
     return origins;
 };
 
