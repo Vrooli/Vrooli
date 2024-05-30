a41fd5158e9fd83677039aec456d7df32285c871
MattHalloran <matthalloran8@gmail.com>
2024-05-30 11:14:03 -0400

    Update useSocketChat.ts
    - Reduced the number of times chat socket event handlers are connected/disconnected due to dependency changes

--- packages/ui/src/hooks/useSocketChat.ts
+++ packages/ui/src/hooks/useSocketChat.ts
@@ -1,7 +1,7 @@
 import { ChatParticipant, ChatSocketEventPayloads, DUMMY_ID, LlmTask, LlmTaskInfo, Session } from "@local/shared";
 import { emitSocketEvent, onSocketEvent } from "api";
 import { SessionContext } from "contexts/SessionContext";
-import { useContext, useEffect, useRef, useState } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { getCurrentUser } from "utils/authentication/session";
 import { getCookieTasksForMessage, removeCookieMatchingChat, setCookieMatchingChat, setCookieTaskForMessage } from "utils/cookies";
 import { PubSub } from "utils/pubsub";
@@ -15,6 +15,7 @@ type UseWebSocketEventsProps = {
     addMessages: (newMessages: ChatMessageShape[]) => unknown;
     chat: ChatShape;
     editMessage: (editedMessage: ChatMessageShape) => unknown;
+    messageTasks: Record<string, LlmTaskInfo[]>;
     participants: ParticipantWithoutChat[];
     removeMessages: (deletedIds: string[]) => unknown;
     setParticipants: (participants: ParticipantWithoutChat[]) => unknown;
@@ -26,10 +27,10 @@ type UseWebSocketEventsProps = {
 }
 
 export const processMessages = (
-    { added, deleted, edited }: ChatSocketEventPayloads['messages'],
-    addMessages: UseWebSocketEventsProps['addMessages'],
-    removeMessages: UseWebSocketEventsProps['removeMessages'],
-    editMessage: UseWebSocketEventsProps['editMessage'],
+    { added, deleted, edited }: ChatSocketEventPayloads["messages"],
+    addMessages: UseWebSocketEventsProps["addMessages"],
+    removeMessages: UseWebSocketEventsProps["removeMessages"],
+    editMessage: UseWebSocketEventsProps["editMessage"],
 ) => {
     if (Array.isArray(added) && added.length > 0) {
         addMessages(added.map(m => ({ ...m, status: "sent" })));
@@ -45,11 +46,11 @@ export const processMessages = (
 };
 
 export const processTypingUpdates = (
-    { starting, stopping }: ChatSocketEventPayloads['typing'],
-    usersTyping: UseWebSocketEventsProps['usersTyping'],
-    participants: UseWebSocketEventsProps['participants'],
+    { starting, stopping }: ChatSocketEventPayloads["typing"],
+    usersTyping: UseWebSocketEventsProps["usersTyping"],
+    participants: UseWebSocketEventsProps["participants"],
     session: Session | undefined,
-    setUsersTyping: UseWebSocketEventsProps['setUsersTyping'],
+    setUsersTyping: UseWebSocketEventsProps["setUsersTyping"],
 ) => {
     // Add every user that's typing
     const newTyping = JSON.parse(JSON.stringify(usersTyping)) as ParticipantWithoutChat[];
@@ -77,11 +78,11 @@ export const processTypingUpdates = (
 };
 
 export const processParticipantsUpdates = (
-    { joining, leaving }: ChatSocketEventPayloads['participants'],
-    participants: UseWebSocketEventsProps['participants'],
-    chat: UseWebSocketEventsProps['chat'],
-    task: UseWebSocketEventsProps['task'],
-    setParticipants: UseWebSocketEventsProps['setParticipants'],
+    { joining, leaving }: ChatSocketEventPayloads["participants"],
+    participants: UseWebSocketEventsProps["participants"],
+    chat: UseWebSocketEventsProps["chat"],
+    task: UseWebSocketEventsProps["task"],
+    setParticipants: UseWebSocketEventsProps["setParticipants"],
 ) => {
     // Remove cache data for old participants group
     const existingUserIds = participants.map(p => p.user.id);
@@ -110,8 +111,10 @@ export const processParticipantsUpdates = (
 
 export const processLlmTasks = (
     { tasks, updates }: ChatSocketEventPayloads["llmTasks"],
+    messageTasks: UseWebSocketEventsProps["messageTasks"],
     updateTasksForMessage: UseWebSocketEventsProps["updateTasksForMessage"],
 ) => {
+    console.log("yeeeet processing new llm tasks", messageTasks, updates);
     // Combine full tasks and updates into a single operation per messageId
     const combinedTasksByMessageId: Record<string, LlmTaskInfo[]> = {};
 
@@ -119,47 +122,67 @@ export const processLlmTasks = (
     (tasks ?? []).forEach(task => {
         if (!task.messageId || !task.id) return;
         if (!combinedTasksByMessageId[task.messageId]) combinedTasksByMessageId[task.messageId] = [];
-        combinedTasksByMessageId[task.messageId].push(task);
+        // Make sure we don't have duplicate tasks
+        const tasksWithoutCurrent = combinedTasksByMessageId[task.messageId].filter(t => t.id !== task.id);
+        combinedTasksByMessageId[task.messageId] = [...tasksWithoutCurrent, task];
     });
 
     // Apply updates
     (updates ?? []).forEach(update => {
-        if (!update.messageId || !update.id) return;
-        if (!combinedTasksByMessageId[update.messageId]) {
-            // If no tasks were initially found for this messageId, initialize the array
-            combinedTasksByMessageId[update.messageId] = [];
+        if (!update.id) return;
+        let messageId = update.messageId;
+        // If messageId not provided, look for it in known tasks
+        if (!messageId) {
+            for (const currTasks of Object.values(messageTasks)) {
+                const matchingTask = currTasks.find(task => task.id === update.id);
+                if (matchingTask) {
+                    messageId = matchingTask.messageId;
+                    break;
+                }
+            }
         }
-        const existingTaskIndex = combinedTasksByMessageId[update.messageId].findIndex(task => task.id === update.id);
+        // If we still don't have a messageId, skip this update
+        if (!messageId) return;
+        console.log("processing llm update", update);
+        if (!combinedTasksByMessageId[messageId]) {
+            // If no tasks were initially found for this messageId, initialize the array
+            combinedTasksByMessageId[messageId] = [];
+        }
+        const existingTaskIndex = combinedTasksByMessageId[messageId].findIndex(task => task.id === update.id);
         if (existingTaskIndex > -1) {
             // Merge the update into the existing task
-            combinedTasksByMessageId[update.messageId][existingTaskIndex] = {
-                ...combinedTasksByMessageId[update.messageId][existingTaskIndex],
+            combinedTasksByMessageId[messageId][existingTaskIndex] = {
+                ...combinedTasksByMessageId[messageId][existingTaskIndex],
                 ...update,
+                messageId,
                 lastUpdated: new Date().toISOString(),
             };
         } else {
             // Handle case where update is for a new task not yet in tasks
-            combinedTasksByMessageId[update.messageId].push({
+            combinedTasksByMessageId[messageId].push({
                 ...update,
+                messageId,
                 lastUpdated: new Date().toISOString(),
             } as LlmTaskInfo);
         }
+        console.log("yeeeet combined tasks", combinedTasksByMessageId);
     });
 
     // Update cache and invoke callback for each messageId using helper functions
     Object.entries(combinedTasksByMessageId).forEach(([messageId, tasksForMessage]) => {
+        console.log("yeeeet applying llm task update for message message", messageId, tasksForMessage);
         tasksForMessage.forEach(task => {
             setCookieTaskForMessage(messageId, task); // Set or update task using helper
         });
         const updatedTasks = getCookieTasksForMessage(messageId);
         if (updatedTasks) {
-            updateTasksForMessage(messageId, updatedTasks.tasks);
+            updateTasksForMessage(messageId, JSON.parse(JSON.stringify(updatedTasks.tasks)));
         }
     });
 };
 
 export const processResponseStream = (
-    { __type, botId, message }: ChatSocketEventPayloads['responseStream'],
+    { __type, botId, message }: ChatSocketEventPayloads["responseStream"],
     messageStreamRef: React.MutableRefObject<ChatSocketEventPayloads["responseStream"] | null>,
     setMessageStream: (stream: ChatSocketEventPayloads["responseStream"] | null) => void,
     throttledSetMessageStream: (stream: ChatSocketEventPayloads["responseStream"] | null) => void,
@@ -205,6 +228,7 @@ export const useSocketChat = ({
     addMessages,
     chat,
     editMessage,
+    messageTasks,
     participants,
     removeMessages,
     setParticipants,
@@ -236,27 +260,32 @@ export const useSocketChat = ({
 
     const messageStreamRef = useRef<ChatSocketEventPayloads["responseStream"] | null>(null);
     const [messageStream, setMessageStream] = useState<ChatSocketEventPayloads["responseStream"] | null>(null);
-    const throttledSetMessageStream = useThrottle((messageStream: ChatSocketEventPayloads["responseStream"] | null) => {
+    const throttledSetMessageStreamHander = useCallback((messageStream: ChatSocketEventPayloads["responseStream"] | null) => {
         // Create copy of message to ensure proper re-rendering
         const messageStreamCopy = messageStream ? { ...messageStream } : null;
         setMessageStream(messageStreamCopy);
-    }, 100);
+    }, []);
+    const throttledSetMessageStream = useThrottle(throttledSetMessageStreamHander, 100);
+
+    // Store refs for parameters to reduce the number of dependencies of socket event handlers. 
+    // This reduces the number of times the socket events are connected/disconnected.
+    const chatRef = useRef(chat);
+    chatRef.current = chat;
+    const messageTasksRef = useRef(messageTasks);
+    messageTasksRef.current = messageTasks;
+    const participantsRef = useRef(participants);
+    participantsRef.current = participants;
+    const taskRef = useRef(task);
+    taskRef.current = task;
+    const usersTypingRef = useRef(usersTyping);
+    usersTypingRef.current = usersTyping;
 
     // Handle incoming data
-    useEffect(() => {
-        const cleanupMessages = onSocketEvent("messages", (payload) => processMessages(payload, addMessages, removeMessages, editMessage));
-        const cleanupTyping = onSocketEvent("typing", (payload) => processTypingUpdates(payload, usersTyping, participants, session, setUsersTyping));
-        const cleanupLlmTasks = onSocketEvent("llmTasks", (payload) => processLlmTasks(payload, updateTasksForMessage));
-        const cleanupParticipants = onSocketEvent("participants", (payload) => processParticipantsUpdates(payload, participants, chat, task, setParticipants));
-        const cleanupResponseStream = onSocketEvent("responseStream", (payload) => processResponseStream(payload, messageStreamRef, setMessageStream, throttledSetMessageStream));
-        return () => {
-            cleanupMessages();
-            cleanupTyping();
-            cleanupLlmTasks();
-            cleanupParticipants();
-            cleanupResponseStream();
-        };
-    }, [addMessages, editMessage, chat.id, participants, removeMessages, session, usersTyping, setUsersTyping, task, updateTasksForMessage, setParticipants]);
+    useEffect(() => onSocketEvent("messages", (payload) => processMessages(payload, addMessages, removeMessages, editMessage)), [addMessages, editMessage, removeMessages]);
+    useEffect(() => onSocketEvent("typing", (payload) => processTypingUpdates(payload, usersTypingRef.current, participantsRef.current, session, setUsersTyping)), [session, setUsersTyping]);
+    useEffect(() => onSocketEvent("llmTasks", (payload) => processLlmTasks(payload, messageTasksRef.current, updateTasksForMessage)), [updateTasksForMessage]);
+    useEffect(() => onSocketEvent("participants", (payload) => processParticipantsUpdates(payload, participantsRef.current, chatRef.current, taskRef.current, setParticipants)), [setParticipants]);
+    useEffect(() => onSocketEvent("responseStream", (payload) => processResponseStream(payload, messageStreamRef, setMessageStream, throttledSetMessageStream)), [throttledSetMessageStream]);
 
     return { messageStream };
 };
