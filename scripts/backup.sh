846aff6fa5099011476a4933a2fb795a7be04fe1
MattHalloran <matthalloran8@gmail.com>
2024-06-13 17:04:26 -0400

    Improved backup script
    - Backup script now stores backups in their own folder, organized by VPS IP address and timestamps
    - Backup script now also stores .env and jwt files, as it would also be bad if these were lost

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script periodically backs up the database from a remote server
+# This script periodically backs up the database and essential files from a remote server
 HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "${HERE}/prettify.sh"
 . "${HERE}/keylessSsh.sh"
@@ -8,12 +8,11 @@ HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 while getopts "c:hi:l:v:" opt; do
     case $opt in
     h)
-        echo "Usage: $0 [-c COUNT] [-h HELP] [-i INTERVAL] [-l LOOP] [-v VERSION]"
+        echo "Usage: $0 [-c COUNT] [-h HELP] [-i INTERVAL] [-l LOOP]"
         echo "  -c --count: The number of most recent backup files to keep"
         echo "  -h --help: Show this help message"
         echo "  -i --interval: The interval in seconds for fetching the logs, if running on a loop"
         echo "  -l --loop: Whether to run this script on a loop, or to exit after one run"
-        echo "  -v --version: Version number of site on server (e.g. \"1.0.0\"). Used to locate the database directory"
         exit 0
         ;;
     c)
@@ -24,9 +23,6 @@ while getopts "c:hi:l:v:" opt; do
         ;;
     l)
         WILL_LOOP=$OPTARG
-        ;;
-    v)
-        VERSION=$OPTARG
         ;;
     \?)
         echo "Invalid option: -$OPTARG" >&2
@@ -53,22 +49,20 @@ if [ -z "$BACKUP_COUNT" ]; then
 fi
 
 if [ -z "$WILL_LOOP" ]; then
-    WILL_LOOP=true
+    WILL_LOOP=false
 fi
 
 # Set the remote server location, using SITE_IP from .env
 remote_server="root@${SITE_IP}"
 info "Remote server: ${remote_server}"
 
-# If no version number is defined, use the version number found in the package.json files
-if [ -z "$VERSION" ]; then
-    info "No version number defined. Using version number found in package.json files."
-    VERSION=$(cat ${HERE}/../packages/ui/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
-    info "Version number found in package.json files: ${VERSION}"
-fi
+# Fetch the version number from the package.json on the remote server
+VERSION=$(ssh -i ~/.ssh/id_rsa_${SITE_IP} $remote_server "cat /root/Vrooli/package.json | grep '\"version\":' | head -1 | awk -F: '{ print \$2 }' | sed 's/[\", ]//g'")
+info "Version number retrieved from remote package.json: ${VERSION}"
 
-# Set the local directory to save the database files to
-local_dir="${HERE}/../data/db-remote-${SITE_IP}-${VERSION}"
+# Set the local directory to save the backup files to
+backup_root_dir="${HERE}/../backups/${SITE_IP}"
+local_dir="${backup_root_dir}/$(date +"%Y%m%d%H%M%S")"
 
 # Set the interval in seconds for fetching the files
 if [ -z "$INTERVAL" ]; then
@@ -76,24 +70,17 @@ if [ -z "$INTERVAL" ]; then
 fi
 
 while true; do
-    # Get the current date and time
-    now=$(date +"%Y-%m-%d-%H-%M-%S")
+    # Create the backup directory
+    mkdir -p "${local_dir}"
 
-    # Set the filename for the backup file
-    backup_filename="db-backup-$now.sql"
+    # Backup the database, data directory, JWT files, and .env* files
+    ssh -i ~/.ssh/id_rsa_${SITE_IP} $remote_server "cd /root/Vrooli && tar -czf - data/postgres-prod jwt_* .env*" >"${local_dir}/backup-$VERSION.tar.gz"
 
-    # Create the backup file using the mysqldump command
-    # ssh $remote_server "pg_dump -Fc -U ${DB_USER}" >"$local_dir/$backup_filename"
-    ssh -i ~/.ssh/id_rsa_${SITE_IP} $remote_server "cd /var/tmp/${VERSION}/data && tar -czf - postgres" >"$local_dir/$backup_filename"
-
-    # Compress the backup file to save disk space
-    gzip "$local_dir/$backup_filename"
-
-    # Remove old backup files to keep only the most recent k backups
-    ls -t "$local_dir"/*.sql.gz | tail -n +$((BACKUP_COUNT + 1)) | xargs rm --
+    # Remove old backup directories to keep only the most recent k backups
+    ls -t "$backup_root_dir" | tail -n +$((BACKUP_COUNT + 1)) | xargs -I {} rm -r "$backup_root_dir"/{}
 
     # Log the backup operation
-    info "Backup created: ${local_dir}/${backup_filename}.gz"
+    info "Backup created: ${local_dir}/backup-$VERSION.tar.gz"
 
     # If not running on a loop, exit the script
     if [ "$WILL_LOOP" = false ]; then
