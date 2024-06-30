0c4ce83247afbf78b0e7d3f2896c1bbe15e07a09
MattHalloran <matthalloran8@gmail.com>
2024-06-29 21:50:51 -0400

    Ad blacklist
    - Added blacklist to hide banner ads on certain pages, such as the landing page

--- packages/ui/src/components/BannerChicken/BannerChicken.tsx
+++ packages/ui/src/components/BannerChicken/BannerChicken.tsx
@@ -1,9 +1,23 @@
+import { LINKS } from "@local/shared";
+import { useTheme } from "@mui/material";
 import { SessionContext } from "contexts/SessionContext";
+import { useWindowSize } from "hooks/useWindowSize";
 import { useContext, useEffect, useMemo } from "react";
+import { useLocation } from "route";
+import { pagePaddingBottom } from "styles";
 import { getCurrentUser } from "utils/authentication/session";
 import { PubSub } from "utils/pubsub";
 
 const HALF = 0.5;
+const BLACKLIST_ROUTES = [
+    LINKS.About,
+    LINKS.ForgotPassword,
+    LINKS.Home,
+    LINKS.Login,
+    LINKS.Pro,
+    LINKS.ResetPassword,
+    LINKS.Signup,
+] as string[];
 
 /**
  * Displays a banner ad the bottom of the screen, above the BottomNav. 
@@ -14,6 +28,9 @@ const HALF = 0.5;
  */
 export function BannerChicken() {
     const session = useContext(SessionContext);
+    const { breakpoints, palette } = useTheme();
+    const [location] = useLocation();
+    const isMobile = useWindowSize(({ width }) => width <= breakpoints.values.md);
 
     const adFrequency = useMemo(() => {
         const user = getCurrentUser(session);
@@ -24,13 +41,15 @@ export function BannerChicken() {
     }, [session]);
 
     const shouldDisplayAd = useMemo(() => {
+        // Don't display ads on certain routes
+        if (BLACKLIST_ROUTES.includes(location.pathname)) return false;
         if (adFrequency === "none") return false;
         if (adFrequency === "full") return true;
         // Pick a random number between 0 and 1
         const random = Math.random();
         // If the random number is less than 0.5, display the ad
         return random < HALF;
-    }, [adFrequency]);
+    }, [adFrequency, location.pathname]);
 
     useEffect(function renderAdEffect() {
         if (!shouldDisplayAd || !process.env.VITE_GOOGLE_ADSENSE_PUBLISHER_ID) {
@@ -51,17 +70,27 @@ export function BannerChicken() {
         };
     }, [shouldDisplayAd]);
 
+    const insStyle = useMemo(() => ({
+        position: "absolute",
+        // Display above the BottomNav, which is only displayed on mobile
+        bottom: isMobile ? pagePaddingBottom : "env(safe-area-inset-bottom)",
+        display: "block",
+        background: palette.background.default,
+    } as const), [isMobile, palette.background.default]);
+
     if (!shouldDisplayAd) return null;
 
     return (
-        <ins className="adsbygoogle"
-            style={{ display: "block" }}
+        <ins
+            className="adsbygoogle"
+            style={insStyle}
             // Disable ads for local development. Ads only work on live domains. 
             // You can use a test domain to test ads before deploying.
             data-adtest={window.location.host.includes("localhost")}
             data-ad-client={`ca-${process.env.VITE_GOOGLE_ADSENSE_PUBLISHER_ID}`}
             data-ad-slot="9649766873"
             data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
+            data-full-width-responsive="true"
+        />
     );
 }
