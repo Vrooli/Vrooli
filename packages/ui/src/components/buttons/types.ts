63dd51a3837ff20a67fbd68db2d700d52caffd25
MattHalloran <matthalloran8@gmail.com>
2025-03-16 13:25:01 -0400

    `SearchButtons` update
    - Improved look and file organization of search filter buttons

--- packages/ui/src/components/buttons/types.ts
+++ packages/ui/src/components/buttons/types.ts
@@ -1,32 +1,11 @@
 import { BookmarkFor, FormSchema, NavigableObject, OrArray, ReactionFor, ReportFor, RunnableProjectVersion, RunnableRoutineVersion, SearchType, Status } from "@local/shared";
 import { ButtonProps } from "@mui/material";
 import React from "react";
-import { FormErrors, SvgProps, SxType, ViewDisplayType } from "types";
+import { FormErrors, SvgProps, SxType, ViewDisplayType } from "../../types.js";
 
 export type AutoFillButtonProps = {
     handleAutoFill: () => unknown;
     isAutoFillLoading: boolean;
-}
-
-export interface AdvancedSearchButtonProps {
-    advancedSearchParams: object | null;
-    advancedSearchSchema: FormSchema | null | undefined;
-    controlsUrl: boolean;
-    searchType: SearchType | `${SearchType}`;
-    setAdvancedSearchParams: (params: object | null) => unknown;
-}
-
-export interface BuildEditButtonsProps {
-    canSubmitMutate: boolean;
-    canCancelMutate: boolean;
-    errors: BottomActionsButtonsProps["errors"];
-    handleCancel: () => unknown;
-    handleScaleChange: (delta: number) => unknown;
-    handleSubmit: () => unknown;
-    isAdding: boolean;
-    isEditing: boolean;
-    loading: boolean;
-    scale: number;
 }
 
 export type CameraButtonProps = {
@@ -83,7 +62,11 @@ export interface HelpButtonProps extends ButtonProps {
 
 export type MicrophoneButtonProps = {
     disabled?: boolean;
+    fill?: string;
+    height?: number;
     onTranscriptChange: (result: string) => unknown;
+    showWhenUnavailable?: boolean;
+    width?: number;
 }
 
 export interface PopupMenuProps extends ButtonProps {
@@ -135,12 +118,6 @@ export interface SideActionsButtonsProps {
     sx?: SxType;
 }
 
-export interface SortButtonProps {
-    options: any; // No way to specify generic enum
-    setSortBy: (sortBy: string) => unknown;
-    sortBy: string;
-}
-
 export interface BookmarkButtonProps {
     disabled?: boolean;
     isBookmarked?: boolean | null; // Defaults to false
@@ -165,11 +142,6 @@ export interface StatusButtonProps extends ButtonProps {
 export type TimeFrame = {
     after?: Date;
     before?: Date;
-}
-
-export interface TimeButtonProps {
-    setTimeFrame: (timeFrame: TimeFrame | undefined) => unknown;
-    timeFrame: TimeFrame | undefined;
 }
 
 export interface VoteButtonProps {
