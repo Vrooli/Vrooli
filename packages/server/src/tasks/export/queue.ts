4b1b54f210d3af69f5fa808bd56355a2dd6d83b2
MattHalloran <matthalloran8@gmail.com>
2025-03-16 17:06:04 -0400

    Importing objects p2

--- packages/server/src/tasks/export/queue.ts
+++ packages/server/src/tasks/export/queue.ts
@@ -1,44 +1,11 @@
-import { SessionUser, Success } from "@local/shared";
+import { Success } from "@local/shared";
 import Bull from "bull";
 import winston from "winston";
-import { DEFAULT_JOB_OPTIONS, LOGGER_PATH, REDIS_CONN_PATH, addJobToQueue, getProcessPath } from "../queueHelper";
+import { ExportConfig } from "../../builders/importExport.js";
+import { DEFAULT_JOB_OPTIONS, LOGGER_PATH, REDIS_CONN_PATH, addJobToQueue, getProcessPath } from "../queueHelper.js";
 
 export type ExportProcessPayload = {
-    /** What data should be exported */
-    flags: {
-        all: boolean;
-        account: boolean;
-        apis: boolean;
-        bookmarks: boolean;
-        bots: boolean;
-        chats: boolean;
-        codes: boolean;
-        comments: boolean;
-        issues: boolean;
-        notes: boolean;
-        //posts: boolean;
-        pullRequests: boolean;
-        projects: boolean;
-        questions: boolean;
-        questionAnswers: boolean;
-        // quizzes: boolean;
-        // quizResponses: boolean; // Includes attempts, answers, etc.
-        reactions: boolean;
-        reminders: boolean;
-        reports: boolean;
-        routines: boolean;
-        runs: boolean;
-        schedules: boolean;
-        standards: boolean;
-        // tags: boolean;
-        /** Includes meetings and roles */
-        teams: boolean;
-        views: boolean;
-    };
-    /** What should happen to the exported data */
-    requestType: "Delete" | "Download" | "DownloadAndDelete";
-    /** The user who's running the command (not the bot) */
-    userData: SessionUser;
+    config: ExportConfig;
 };
 
 let logger: winston.Logger;
@@ -50,7 +17,7 @@ const FOLDER = "export";
 export async function setupExportQueue() {
     try {
         logger = (await import(LOGGER_PATH)).logger;
-        const REDIS_URL = (await import(REDIS_CONN_PATH)).REDIS_URL;
+        const REDIS_URL = (await import(REDIS_CONN_PATH)).getRedisUrl();
         exportProcess = (await import(getProcessPath(FOLDER))).exportProcess;
 
         // Initialize the Bull queue
@@ -69,6 +36,6 @@ export async function setupExportQueue() {
     }
 }
 
-export function exportData(data: ExportProcessPayload): Promise<Success> {
+export function startDataExport(data: ExportProcessPayload): Promise<Success> {
     return addJobToQueue(exportQueue, data, {});
 }
