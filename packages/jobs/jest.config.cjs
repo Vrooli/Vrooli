1cb14aad3a14c0ece24ee8df6f7c49cc014e8e21
MattHalloran <matthalloran8@gmail.com>
2025-03-16 16:37:28 -0400

    Linting and package script updates
    - Updated test scripts to include `.env-test` environment variables
    - Updated build script so that it uses the same output directory as tests, but safely omits test files
    - Updated linting rules to force file extensions
    - Simplified how additional files are copied after building the server

--- packages/jobs/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    roots: ["<rootDir>/src"],
-    testPathIgnorePatterns: ["/node_modules/", "/dist/", "/rust/", "/generated/"],
-    coveragePathIgnorePatterns: ["/node_modules/", "/dist/", "/rust/", "/generated/"],
-};
