c8c148411a20e2a2f5d792b4cae5abc7faca960f
MattHalloran <matthalloran8@gmail.com>
2025-05-28 20:12:38 -0400

    packages/shared unit tests fixed and passing!

--- packages/shared/src/__test/setup.ts
+++ packages/shared/src/__test/setup.ts
@@ -1,37 +1,80 @@
 import { JSDOM } from "jsdom";
 
-// Create a basic DOM environment
-const dom = new JSDOM("<!doctype html><html><body></body></html>", {
-    url: "http://localhost", // Define a non-opaque origin for localStorage to work
-});
+const originalGlobals: { [key: string]: any } = {};
 
-// Since the shared package is used in both the server and the client,
-// we let individual tests decide whether to set up a DOM environment.
-export function setupDOM() {
-    // Set global variables to mimic a browser
-    global.window = dom.window;
-    global.document = dom.window.document;
-    global.navigator = {
-        userAgent: "node.js",
-    } as Navigator;
-    global.localStorage = dom.window.localStorage;
-    global.sessionStorage = dom.window.sessionStorage;
+// Properties JSDOM might introduce that we'll manage.
+const jsdomGlobalProperties = [
+    "window", "document", "navigator", "localStorage", "sessionStorage",
+    "Node", "Element", "HTMLElement", "HTMLDocument", "Event", "CustomEvent", "MouseEvent",
+    "DocumentFragment", "Text", "Comment", "CDATASection", "ProcessingInstruction",
+    "Attr", "NamedNodeMap", "DOMTokenList", "NodeList", "HTMLCollection",
+    "URL", "URLSearchParams", "AbortController", "AbortSignal",
+];
 
-    // Attempt to copy other window properties to global,
-    // but skip any that are read-only.
-    Object.getOwnPropertyNames(dom.window).forEach((property) => {
-        if (typeof global[property] === "undefined") {
-            try {
-                global[property] = dom.window[property];
-            } catch (err) {
-                // Ignore properties that cannot be assigned
+let currentDOM: JSDOM | null = null;
+
+export function setupDOM(initialUrl = "http://localhost") {
+    // If a DOM already exists from a previous unterminated test, try to clean it.
+    if (currentDOM) {
+        console.warn("JSDOM instance found at start of setupDOM. Tearing down previous instance.");
+        teardownDOM(); // Attempt to clean up previous state
+    }
+
+    currentDOM = new JSDOM("<!doctype html><html><body></body></html>", {
+        url: initialUrl,
+        runScripts: "dangerously",
+        pretendToBeVisual: true,
+    });
+    const { window: jsdomWindow } = currentDOM;
+
+    const propertiesToManage = [...new Set([...jsdomGlobalProperties, "window", "document", "navigator", "location", "history"])];
+
+    propertiesToManage.forEach(prop => {
+        if (Object.prototype.hasOwnProperty.call(global, prop) && !Object.prototype.hasOwnProperty.call(originalGlobals, prop)) {
+            originalGlobals[prop] = (global as any)[prop];
+        }
+
+        if (Object.prototype.hasOwnProperty.call(jsdomWindow, prop)) {
+            Object.defineProperty(global, prop, {
+                value: (jsdomWindow as any)[prop],
+                writable: true,
+                enumerable: true,
+                configurable: true,
+            });
+        } else {
+            if (Object.prototype.hasOwnProperty.call(global, prop) && !Object.prototype.hasOwnProperty.call(originalGlobals, prop)) {
+                delete (global as any)[prop];
             }
         }
     });
 }
 
 export function teardownDOM() {
-    if (global.window && typeof global.window.close === "function") {
-        global.window.close();
+    const propertiesToCleanup = [...new Set([...jsdomGlobalProperties, "window", "document", "navigator", "location", "history"])];
+
+    propertiesToCleanup.forEach(prop => {
+        if (Object.prototype.hasOwnProperty.call(originalGlobals, prop)) {
+            Object.defineProperty(global, prop, {
+                value: originalGlobals[prop],
+                writable: true,
+                enumerable: true,
+                configurable: true,
+            });
+        } else if (Object.prototype.hasOwnProperty.call(global, prop)) {
+            // Only delete if it was not an original global property
+            delete (global as any)[prop];
+        }
+    });
+
+    if (currentDOM) {
+        currentDOM.window.close();
+        currentDOM = null;
+    }
+
+    // Clear originalGlobals for the next test.
+    for (const key in originalGlobals) {
+        if (Object.prototype.hasOwnProperty.call(originalGlobals, key)) {
+            delete originalGlobals[key];
+        }
     }
 }
