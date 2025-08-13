0cb9bcec3c25cc09319deb7d8ca68695f38728b4
Matthew Halloran <matthalloran8@gmail.com>
2025-08-12 22:21:49 -0400

    staging packages/ for removal p15

--- platforms/cli/src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export { ApiClient } from "./client.js";
-export { ConfigManager } from "./config.js";
-export type { ProfileConfig, CliConfig } from "./config.js";
-export type { ApiError } from "./client.js";
-export type { CliOptions } from "./types.js";
