3709006dd7cab7b9b952e59329e9c21851b814dd
Matthew Halloran <matthalloran8@gmail.com>
2025-09-10 03:20:26 -0400

    updated scenario clis p2

--- scenarios/campaign-content-studio/cli/install.sh
+++ scenarios/campaign-content-studio/cli/install.sh
@@ -5,4 +5,4 @@ APP_ROOT="${APP_ROOT:-$(builtin cd "${BASH_SOURCE[0]%/*}/../../.." && builtin pw
 CLI_DIR="${APP_ROOT}/scenarios/campaign-content-studio/cli"
 source "${APP_ROOT}/scripts/lib/utils/cli-install.sh"
 
-install_cli "$CLI_DIR/campaign-content-studio-cli.sh" "campaign-content-studio"
+install_cli "$CLI_DIR/campaign-content-studio.sh" "campaign-content-studio"
