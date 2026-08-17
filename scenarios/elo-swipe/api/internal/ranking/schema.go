66c0f29206de3499ef8cc1a31f952d1decf7a6ac
Matthew Halloran <matthalloran8@gmail.com>
2026-08-17 11:03:18 -0400

    Old scenario cleanup

--- scenarios/elo-swipe/api/internal/ranking/schema.go
+++
@@ -1,9 +0,0 @@
-package ranking
-
-import _ "embed"
-
-//go:embed schema.sql
-var schemaSQL string
-
-// Schema returns the scenario-owned database schema.
-func Schema() string { return schemaSQL }
