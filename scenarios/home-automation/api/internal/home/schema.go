bf6d465c579184587a03054b5073df5d5d788f04
Matthew Halloran <matthalloran8@gmail.com>
2026-08-17 19:21:15 -0400

    iot p2

--- scenarios/home-automation/api/internal/home/schema.go
+++
@@ -1,9 +0,0 @@
-package home
-
-import _ "embed"
-
-//go:embed schema.sql
-var schemaSQL string
-
-// Schema returns the home domain schema applied during API startup.
-func Schema() string { return schemaSQL }
