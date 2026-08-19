6c6ebbe2ca136b114c53d402b38d8b9473e879d8
Matthew Halloran <matthalloran8@gmail.com>
2026-08-18 23:55:29 -0400

    Old scenario cleanup p3

--- scenarios/symbol-search/api/internal/symbols/schema.go
+++
@@ -1,9 +0,0 @@
-package symbols
-
-import _ "embed"
-
-//go:embed schema.sql
-var schemaSQL string
-
-// Schema returns the symbols domain schema applied during API startup.
-func Schema() string { return schemaSQL }
